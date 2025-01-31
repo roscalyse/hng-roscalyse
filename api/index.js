39bd18677fd6fa96e2eb320ec2d5683211cec768
Namukasa Proscovia <127551337+roscalyse@users.noreply.github.com>
2025-01-31 12:30:09 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 export default (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({
-    email: "your-email@example.com",
+    email: "roscalyse11@gmail.com",
     current_datetime: new Date().toISOString(),
     github_url: "https://github.com/roscalyse/hng-roscalyse.git"
   });
