4e52618fb103c3bf45ad24e8d8b52f6146442aeb
Namukasa Proscovia <127551337+roscalyse@users.noreply.github.com>
2025-01-31 12:40:19 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,10 +2,10 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-$result =[
+echo json_encode([
     'email_address'=> 'roscalyse11@gmail.com',
     'current_datetime'=> gmdate("c"),
     'github_url' => 'https://github.com/roscalyse/hng-roscalyse.git'
-];
-echo json_encode($result);
+]);
+
 ?>
