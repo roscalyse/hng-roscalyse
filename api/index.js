ce09e4fe1f1e5dfb11b8225089ceb99748ad9462
Namukasa Proscovia <127551337+roscalyse@users.noreply.github.com>
2025-01-31 12:31:40 +0300

    Update and rename index.js to index.php

--- api/index.js
+++
@@ -1,9 +0,0 @@
-
-export default (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({
-    email: "roscalyse11@gmail.com",
-    current_datetime: new Date().toISOString(),
-    github_url: "https://github.com/roscalyse/hng-roscalyse.git"
-  });
-};
