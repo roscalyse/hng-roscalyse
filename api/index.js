8032ada477f60dd44a908ac8b3b1e1b99eb5b063
Namukasa Proscovia <127551337+roscalyse@users.noreply.github.com>
2025-01-31 13:11:54 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 export default (req, res) => {
+  // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  // Allow GET method
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  //json Content-Type
   res.setHeader('Content-Type', 'application/json');
+  
   res.status(200).json({
     email: "roscalyse11@gmail.com",
     current_datetime: new Date().toISOString(),
